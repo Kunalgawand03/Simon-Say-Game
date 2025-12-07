2e1547f4307a24a6156a7e3752ef76b3474a193c
Kunal Gawand <143700746+Kunalgawand03@users.noreply.github.com>
2025-12-07 23:26:10 +0530

    Implement game sequence and user answer validation
    Added game sequence tracking and answer checking logic.

--- app.js
+++ app.js
@@ -1,56 +1,76 @@
-let gameseq=[];
-let userseq=[];
+let gameseq = [];
+let userseq = [];
 
 let btns = ["yellow", "red", "purple", "green"];
-
-let started=false;
-let level=0;            
+let started = false;
+let level = 0;
 
 let h2 = document.querySelector("h2");
 
-document.addEventListener("keydown", function() {
-    if (started == false) {
-        console.log("Game started");
+document.addEventListener("keydown", function () {
+    if (!started) {
         started = true;
-
         levelUp();
     }
-}); 
+});
 
 function levelUp() {
+    userseq = [];         // empty user sequence each level
     level++;
-    h2.innerText=`Level ${level}`;
+    h2.innerText = `Level ${level}`;
 
+    // random button
     let randIdx = Math.floor(Math.random() * 4);
     let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`);
-    console.log(randIdx);
-    console.log(randColor);
-    console.log(randBtn);
+
+    gameseq.push(randColor);
     gameFlash(randBtn);
 }
 
-function gameFlash(btn){
+function checkAns(idx) {
+    if (userseq[idx] === gameseq[idx]) {
+        if (userseq.length === gameseq.length) {
+            setTimeout(levelUp, 700);
+        }
+    } else {
+        h2.innerHTML = `Game Over! Press any key to restart`;
+        resetGame();
+    }
+}
+
+function gameFlash(btn) {
     btn.classList.add("flash");
-    setTimeout(function() {
+    setTimeout(function () {
         btn.classList.remove("flash");
     }, 250);
 }
 
-function userFlash(btn){
+function userFlash(btn) {
     btn.classList.add("userFlash");
-    setTimeout(function() {
+    setTimeout(function () {
         btn.classList.remove("userFlash");
     }, 250);
 }
 
 function btnPress() {
-    console.log(this);
     let btn = this;
+    let btnColor = btn.classList[1];
+
     userFlash(btn);
+    userseq.push(btnColor);
+
+    checkAns(userseq.length - 1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
-for (btn of allBtns) {
-    btn.addEventListener("click", btnPress)
+for (let btn of allBtns) {
+    btn.addEventListener("click", btnPress);
+}
+
+function resetGame() {
+    started = false;
+    gameseq = [];
+    userseq = [];
+    level = 0;
 }
